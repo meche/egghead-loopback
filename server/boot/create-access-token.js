b367cfbbdd33b73912ec480e1f2cfcaa4752e5d2
Leandro Alexandrino <leandro@leandrino.me>
2017-06-28 01:05:44 -0300

    create a access token to tests

--- server/boot/create-access-token.js
+++ server/boot/create-access-token.js
@@ -1,12 +1,19 @@
 'use strict';
 
+const Promise = require('bluebird');
+
 module.exports = function(app, cb) {
-  /*
-   * The `app` object provides access to a variety of LoopBack resources such as
-   * models (e.g. `app.models.YourModelName`) or data sources (e.g.
-   * `app.datasources.YourDataSource`). See
-   * http://docs.strongloop.com/display/public/LB/Working+with+LoopBack+objects
-   * for more info.
-   */
-  process.nextTick(cb); // Remove if you pass `cb` to an async function yourself
+  const AccessToken = app.models.AccessToken;
+  const User = app.models.User;
+
+  const email = 'admin@example.com';
+  const password = 'secr3t';
+  const accessToken = 'secr3t';
+
+  return Promise.resolve()
+    .then(() => User.findOne({where: {email}}))
+    .then(res => (res ? res : User.create({email, password})))
+    .then(user => AccessToken.upsert({id: accessToken, userId: user.id}))
+    .then(token => console.log('Access token: ', token.id))
+    .asCallback(cb);
 };
